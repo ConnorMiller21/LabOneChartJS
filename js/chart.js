f7368223b9afa96e2c7e25869e4fedfe6514e006
ConnorMiller21 <miller.connor95@gmail.com>
2018-02-06 11:02:42 -0600

    backend improvements

--- js/chart.js
+++ js/chart.js
@@ -1,9 +1,5 @@
 function displayfileData(allData) {
 
-   //console.log(allData);
-
-   //document.querySelector("#chart-container").innerHTML = "<h2 class='Title'>Books</h2>";
-
    allData.forEach( (book) => {
       let title = book.title;
       let author = book.author;
@@ -23,51 +19,54 @@ function displayfileData(allData) {
 }
 
 function avePriceByFormat(allData) {
-
    //console.log(allData);
-
-   var numOfEbooks = 0;
-   var ebookAcc = 0;
-
-   var numOfHardcovers = 0;
-   var hardcoverAcc = 0;
-
-   var numOfPaperBacks = 0;
-   var paperbackAcc = 0;
+   var ebookArr = [];
+   var hardcoverArr = [];
+   var paperbackArr = [];
 
    allData.forEach( (book) => {
       //console.log(book.format);
       switch (book.format) {
          case "ebook":
             //console.log("in E!");
-            numOfEbooks += 1;
-            ebookAcc += book.price;
+            ebookArr.push(book.price);
             break;
          case "hardcover":
             //console.log("in H!");
-            numOfHardcovers += 1;
-            hardcoverAcc += book.price;
+            hardcoverArr.push(book.price);
             break;
          case "paperback":
             //console.log("in P!");
-            numOfPaperBacks += 1;
-            paperbackAcc += book.price;
+            paperbackArr.push(book.price);
+            break;
+         default:
             break;
       }
    });
 
-   var aveEbookPrice = (ebookAcc / numOfEbooks).toFixed(2);
-   var aveHardcoverPrice = (hardcoverAcc / numOfHardcovers).toFixed(2);
-   var avePaperbackPrice = (paperbackAcc / numOfPaperBacks).toFixed(2);
-   //console.log("Average Price of Ebook", aveEbookPrice);
-   //console.log("Average Price of Hardcover", aveHardcoverPrice);
-   //console.log("Average Price of Paperback", avePaperbackPrice);
+   console.log(ebookArr, hardcoverArr, paperbackArr);
+
+   var aveEbookPrice = getArrayAverage(ebookArr);
+   var aveHardcoverPrice = getArrayAverage(hardcoverArr);
+   var avePaperbackPrice = getArrayAverage(paperbackArr);
+
+   console.log(aveEbookPrice, aveHardcoverPrice, avePaperbackPrice);
+
    return {
       "title": "Average Price of Book By Book Format",
       "key": "Price",
       "bookFormat": ["ebook", "hardcover", "paperback"],
       "avePrice": [aveEbookPrice, aveHardcoverPrice, avePaperbackPrice]
    };
+}
+
+function getArrayAverage(arr) {
+   total = 0;
+   arr.forEach( (price) => {
+      total += price;
+   });
+   var average = (total / arr.length).toFixed(2);
+   return average
 }
 
 function createChartToDOM(titleInfo, xLabelInfo, legendKeyLabel, dataInfo) {
